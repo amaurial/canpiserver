fbd9afccad93b08d3da338f12a55dc9bda7668d1
Amauri <amaurial@uol.com.br>
2017-02-23 21:58:57 +1300

    Fix webpage for new configuration Fix the scripts to cope with the simple structure

--- start_canpi.sh
+++ start_canpi.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 ### BEGIN INIT INFO
-# Provides: canpi
+# Provides: canpiserver
 # Required-Start:    $remote_fs $syslog $network
 # Required-Stop:     $remote_fs $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: Start Merg canpi
-# Description:       Enable service provided by Merg canpi daemon.
+# Short-Description: Start Merg canpiserver
+# Description:       Enable service provided by Merg canpiserver daemon.
 ### END INIT INFO
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 . /lib/lsb/init-functions
 
-dir="/home/pi/canpi"
-cmd="/home/pi/canpi/canpi"
+dir="/home/pi/canpiserver"
+cmd="/home/pi/canpiserver/canpiserver"
 webdir="$dir/webserver"
 upgradedir="$dir/upgrade"
 webcmd="/usr/bin/python $webdir/canpiconfig.py 80"
@@ -50,29 +50,6 @@ else
         echo "Failed to create 'upgrade' directory"
     fi
 fi
-
-#Bonjour files
-bonjour_file="/etc/avahi/services/multiple.service"
-bonjour_template="$dir/multiple.service"
-
-#wpa supplicant files
-wpa_file="/etc/wpa_supplicant/wpa_supplicant.conf"
-wpa_template="$dir/wpa_supplicant.conf"
-
-#hostapd files
-hostapd_file="/etc/hostapd/hostapd.conf"
-hostapd_template="$dir/hostapd.conf"
-default_hostapd_file="/etc/default/hostapd"
-safe_hostapd_file="$dir/hostapd"
-
-#dhcp files
-default_dhcp_file="/etc/default/isc-dhcp-server"
-safe_dhcp_file="$dir/isc-dhcp-server"
-
-#interface files
-iface_file="/etc/network/interfaces"
-iface_file_wifi="$dir/interfaces.wifi"
-iface_file_ap="$dir/interfaces.ap"
 
 get_push_button(){
    pb=`grep button_pin ${config} |cut -d "=" -f 2`
@@ -112,27 +89,6 @@ blink_red_led(){
    unset_red_led
    sleep 1
    set_red_led
-}
-
-reconfigure_if_pb_pressed(){
-   echo "Checking if the push button is pressed"
-   p=$(is_pb_pressed)
-   if [[ $p -eq "0" ]];then
-      #we reconfigure
-      setup_red_led
-      blink_red_led
-      unset_red_led
-      echo "Push button pressed. Reconfiguring"
-      ap_no_password="true"
-      sed -i 's/ap_mode="false"/ap_mode="true"/Ig' $config
-      sed -i 's/ap_no_password=false/ap_no_password="true"/Ig' $config
-      setup_ap_mode
-      setup_bonjour
-      sleep 1
-      echo "Rebooting"
-      sudo reboot
-      exit 0
-   fi
 }
 
 get_red_led_pin(){
@@ -181,35 +137,14 @@ get_web_pid() {
     cat "$web_pid_file"
 }
 
-is_wifi_running(){
-   #the wifi is running if we have and ip address
-   ipaddr=`ip addr | grep -A2 'wlan0:' |grep -A2 'state UP' -A2 | tail -n1 | awk '{print $2}'`
-   [[ $ipaddr =~ [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\/[0-9]+ ]]
-   echo $?
-}
-
 is_web_running() {
     [ -f "$web_pid_file" ] && ps `get_web_pid` > /dev/null 2>&1
 }
 
-is_avahi_running(){
-    r=`/etc/init.d/avahi-daemon status`
-    echo $r | grep "active (running)"
-}
-
-is_dhcp_running(){
-    r=`/etc/init.d/isc_dhcp-server status`
-    echo $r | grep "active (running)"
-}
-
-is_hostapd_running(){
-    r=`/etc/init.d/hostapd status`
-    echo $r | grep "active (running)"
-}
 
 kill_all_processes(){
     p="$1"
-    list=`ps -A -o pid,cmd |grep "$p" | grep -v color | grep -v start_canpi | cut -d " " -f 2`
+    list=`ps -A -o pid,cmd |grep "$p" | grep -v color | grep -v start_canpiserver | cut -d " " -f 2`
     for pid in $list;
     do
         echo "Killing process $pid"
@@ -217,169 +152,7 @@ kill_all_processes(){
     done
 }
 
-setup_bonjour() {
-    echo "Configuring the bonjour service"
-    #back the old file
-    #mv $bonjour_file "${bonjour_file}.bak"
-    cp -f $bonjour_template "${bonjour_template}.tmp"
 
-    #change the service name
-    sed -i 's/SERVICENAME/'"$service_name"'/' "${bonjour_template}.tmp"
-
-    #change the port
-    sed -i 's/PORT/'"$tcpport"'/' "${bonjour_template}.tmp"
-    mv -f "${bonjour_template}.tmp" $bonjour_file
-
-    #restart the service
-    #echo "Restarting the bonjour service"
-    #/etc/init.d/avahi-daemon restart
-    #sleep 1
-    #r=`/etc/init.d/avahi-daemon status`
-    #echo $r | grep "active (running)"
-    if is_avahi_running;
-    then
-        echo "Bonjour restarted"
-    else
-        echo "Failed to restart Bonjour"
-    fi
-}
-
-setup_wpa_supplicant(){
-    echo "Configuring the WPA supplicant"
-    #back the old file
-    sudo mv -f $wpa_file "${wpa_file}.bak"
-    sudo cp -f $wpa_template $wpa_file
-
-    #change the ssid
-    sudo sed -i 's/SSID/'"$router_ssid"'/' $wpa_file
-
-    #change the password
-    sudo sed -i 's/PASSWORD/'"$router_password"'/' $wpa_file
-
-}
-
-setup_hostapd(){
-    echo "Configuring the hostapd"
-    #back the old file
-    sudo mv -f $hostapd_file "${hostapd_file}.bak"
-    sudo cp -f $hostapd_template $hostapd_file
-
-    #change the ssid
-    sudo sed -i 's/SSID/'"$ap_ssid"'/' $hostapd_file
-
-    #change the password
-    sudo sed -i 's/PASSWORD/'"$ap_password"'/' $hostapd_file
-
-    #change the channel
-    sudo sed -i 's/CHANNEL/'"$ap_channel"'/' $hostapd_file
-}
-
-setup_hostapd_no_password(){
-    echo "Configuring the hostapd with no password"
-    #back the old file
-    sudo mv -f $hostapd_file "${hostapd_file}.bak"
-    sudo cp -f "${hostapd_template}.nopassword" $hostapd_file
-
-    #change the ssid
-    sudo sed -i 's/SSID/'"$ap_ssid"'/' $hostapd_file
-
-    #change the channel
-    sudo sed -i 's/CHANNEL/'"$ap_channel"'/' $hostapd_file
-}
-
-
-
-setup_iface_wifi(){
-    #back the old file
-    sudo mv -f $iface_file "${iface_file}.bak"
-    sudo cp -f $iface_file_wifi $iface_file
-}
-
-setup_iface_ap(){
-    #back the old file
-    sudo mv -f $iface_file "${iface_file}.bak"
-    sudo cp -f $iface_file_ap $iface_file
-}
-
-remove_hostapd_default(){
-    #back the old file
-    sudo mv -f $default_hostapd_file "${default_hostapd_file}.bak"
-}
-
-remove_dhcp_default(){
-    #back the old file
-    sudo mv -f $default_dhcp_file "${default_dhcp_file}.bak"
-}
-
-restore_hostapd_default(){
-    #back the old file
-    sudo cp -f $safe_hostapd_file $default_hostapd_file
-}
-
-restore_dhcp_default(){
-    #back the old file
-    sudo cp -f $safe_dhcp_file $default_dhcp_file
-}
-
-setup_ap_mode()
-{
-    echo "Configuring to AP mode"
-
-    sleep 1
-    sudo /etc/init.d/hostapd stop
-    sleep 1
-    sudo /etc/init.d/isc-dhcp-server stop
-
-
-    echo "Restarting wlan0"
-    sudo ip link set wlan0 down
-    setup_iface_ap
-    if [[ $ap_no_password == "true" || $ap_no_password == "True" || $ap_no_password == "TRUE" ]];then
-        setup_hostapd_no_password
-    else
-        setup_hostapd
-    fi
-
-    sleep 1
-    sudo ip link set wlan0 up
-
-    sleep 2
-    sudo /etc/init.d/hostapd start
-    sleep 1
-    sudo /etc/init.d/isc-dhcp-server start
-
-    echo "Configuring the hostapd daemon"
-    sudo /usr/sbin/update-rc.d hostapd defaults
-    echo "Configuring DHCP server"
-    sudo /usr/sbin/update-rc.d isc-dhcp-server defaults
-}
-
-setup_wifi_mode(){
-    echo "Configuring to wifi mode"
-    setup_iface_wifi
-    setup_wpa_supplicant
-
-    echo "Stoping wlan0"
-    sudo ip link set wlan0 down
-
-    echo "Stoping the hostapd service"
-    sudo /etc/init.d/hostapd stop
-    sleep 1
-
-    echo "Stoping the DHCP service"
-    sudo /etc/init.d/isc-dhcp-server stop
-    sleep 1
-
-    #echo "Starting wlan0"
-    #sudo ip link set wlan0 up
-    #sudo /etc/init.d/networking rstart
-    #sleep 1
-    #remove_hostapd_default
-    #remove_dhcp_default
-    sudo /usr/sbin/update-rc.d hostapd remove
-    sleep 1
-    sudo /usr/sbin/update-rc.d isc-dhcp-server remove
-}
 
 start_led_watchdog(){
    echo "watchdog"
@@ -450,10 +223,10 @@ start_canpi(){
 }
 
 upgrade_canpi(){
-    echo "Checking for file 'canpi'"
-    if [[ -f canpi ]];then
+    echo "Checking for file 'canpiserver'"
+    if [[ -f canpiserver ]];then
 
-        echo "'canpi' file present. Stopping the service"
+        echo "'canpiserver' file present. Stopping the service"
 
         stop_canpi
         if [[ $? -eq 1 ]]; then
@@ -462,9 +235,9 @@ upgrade_canpi(){
 
         echo "Service stopped. Backing up the file"
         cd "${upgradedir}"
-        mv -f ../canpi ../canpi.bkp
-        cp -f canpi ../
-        chmod +x ../canpi
+        mv -f ../canpiserver ../canpiserver.bkp
+        cp -f canpiserver ../
+        chmod +x ../canpiserver
 
         echo "Starting the service after upgrade"
         start_canpi
@@ -472,8 +245,8 @@ upgrade_canpi(){
         if [[ $? -eq 1 ]]; then
             echo "Failed to restart the service. Reversing the upgrade"
             cd "${upgradedir}"
-            mv -f ../canpi.bkp ../canpi
-            chmod +x ../canpi
+            mv -f ../canpiserver.bkp ../canpiserver
+            chmod +x ../canpiserver
             start_canpi
             if [[ $? -eq 1 ]]; then
                 echo "Failed to start the service after recover"
@@ -484,7 +257,7 @@ upgrade_canpi(){
         fi
         echo "Service restarted after upgrade."
     else
-        echo "No 'canpi' file on upgrade files"
+        echo "No 'canpiserver' file on upgrade files"
     fi
     return 0
 }
@@ -564,15 +337,6 @@ copy_start_script_file(){
 
 upgrade_config_files(){
     cd "${upgradedir}"
-    copy_config_file dhcpd.conf
-    copy_config_file hostapd
-    copy_config_file hostapd.conf
-    copy_config_file hostapd.conf.nopassword
-    copy_config_file interfaces.ap
-    copy_config_file interfaces.wifi
-    copy_config_file isc-dhcp-server
-    copy_config_file multiple.service
-    copy_config_file wpa_supplicant.conf
     copy_start_script_file start_canpi.sh
 }
 
@@ -592,7 +356,7 @@ apply_upgrade(){
     if [[ -d "${upgradedir}" && ! -L "${upgradedir}" ]] ; then
         echo "'upgrade' directory exists. Checking for upgrade files."
         cd "${upgradedir}"
-        listfiles=`ls -t | grep ".zip" | grep canpiwi-upgrade`
+        listfiles=`ls -t | grep ".zip" | grep canpiserver-upgrade`
         upfile=(${listfiles[@]})
         echo "Upgrade zip files: ${upfile}"
         if [[ -f "${upfile}" ]] ; then
@@ -669,15 +433,8 @@ set_push_button
 
 case "$1" in
     start)
-        #reconfigure if the push button is pressed
-        reconfigure_if_pb_pressed
         setup_red_led
-        unset_red_led
-        if [[ is_wifi_running -eq 0 ]] ; then
-           echo "We found a valid ip. Wifi is running"
-           set_red_led
-        fi
-        setup_bonjour
+        set_red_led
         start_webserver
         start_canpi
         if [[ $? -eq 1 ]]; then
@@ -688,11 +445,10 @@ case "$1" in
         stop_webserver
         stop_canpi
         #kill the rest
-        kill_all_processes "canpi"
+        kill_all_processes "canpiserver"
     ;;
     startcanpi)
         setup_red_led
-        setup_bonjour
         start_canpi
         if [[ $? -eq 1 ]]; then
             exit 1
@@ -705,7 +461,6 @@ case "$1" in
         fi
     ;;
     restartcanpi)
-        #$0 stopcanpi
         stop_canpi
         if [[ $? -eq 1 ]]; then
             exit 1
@@ -714,7 +469,6 @@ case "$1" in
             echo "Unable to stop, will not attempt to start"
             exit 1
         fi
-        #$0 startcanpi
         start_canpi
         if [[ $? -eq 1 ]]; then
             exit 1
@@ -728,21 +482,6 @@ case "$1" in
         fi
         $0 start
     ;;
-    configure)
-        echo "Configuring the services"
-        blink_red_led
-        if [[ $ap_mode == "true" || $ap_mode == "True" || $ap_mode == "TRUE" ]];then
-           echo "Starting configuration for AP Mode"
-           setup_ap_mode
-        else
-           echo "Starting configuration for Wifi Mode"
-           setup_wifi_mode
-        fi
-        setup_bonjour
-        sleep 1
-        echo "Rebooting"
-        sudo reboot
-    ;;
     upgrade)
         echo "Check for upgrade"
         apply_upgrade
@@ -755,18 +494,14 @@ case "$1" in
         fi
 
         if is_running; then
-            echo "Canpi is Running"
+            echo "Canpiserver is Running"
         else
-            echo "Canpi Stopped"
+            echo "Canpiserver Stopped"
             exit 1
         fi
     ;;
-    shutdown)
-        echo "Shutting down"
-        sudo halt 
-    ;;
     *)
-    echo "Usage: $0 {start|stop|restart|status|configure|startcanpi|stopcanpi|restartcanpi|shutdown|upgrade}"
+    echo "Usage: $0 {start|stop|restart|status|startcanpi|stopcanpi|restartcanpi|upgrade}"
     exit 1
     ;;
 esac
--- webserver/canpiconfig.py
+++ webserver/canpiconfig.py
@@ -8,8 +8,8 @@ import shlex
 import re
 from subprocess import Popen, PIPE
 
-configpath="/home/pi/canpi/canpi.cfg"
-messageFile="/home/pi/canpi/webserver/msg"
+configpath="/home/pi/canpiserver/canpi.cfg"
+messageFile="/home/pi/canpiserver/webserver/msg"
 portlimit = 65535
 invalid_ports = [21,22,80]
 
@@ -78,7 +78,7 @@ class configManager:
             for k,v in self.config.iteritems():
                 line = ""
                 #dont put quotes on numbers
-                if k in ["tcpport","cangrid_port","ap_channel","node_number","canid","button_pin","green_led_pin","yellow_led_pin","red_led_pin","start_event_id","orphan_timeout","shutdown_code"]:
+                if k in ["cangrid_port","canid","button_pin","green_led_pin","yellow_led_pin","red_led_pin","start_event_id","shutdown_code"]:
                      line = k + "=" + v + "\n"
                 else:
                      line = k + "=\"" + v + "\"\n"
@@ -93,76 +93,31 @@ class configManager:
 cm = configManager()
 cm.readConfig()
 
-id_apmode="apmode"
-id_apmode_no_passwd="ap_no_password"
-id_ssid="ap_ssid"
-id_password="ap_password"
-id_channel="ap_channel"
-id_router_ssid="router_ssid"
-id_router_password="router_password"
-id_bonjour_name="service_name"
-id_ed_tcpport="tcpport"
-id_grid_enable="gridenable"
 id_grid_port="cangrid_port"
 id_logfile="logfile"
 id_loglevel="loglevel"
 id_logappend="logappend"
 id_canid="canid"
-id_turnout_file="turnout_file"
-id_fns_momentary="fn_momentary"
 id_create_logfile="create_log_file"
 id_start_event_id="start_event_id"
 id_btn_save="btnSave"
-id_btn_apply="btnApply"
 id_btn_restart="btnRestart"
 id_btn_stop="btnStop"
 id_btn_restart_all="btnRestartAll"
 id_btn_update_file="btnUpdateFile"
-id_btn_shutdown="btnShutdown"
-id_orphan_timeout="orphan_timeout"
-id_shutdown_code="shutdown_code"
 
-desc_apmode="AP mode"
-desc_apmode_no_passwd="No password in AP mode"
-desc_ssid="SSID"
-desc_password="Password"
-desc_channel="Channel"
-desc_router_ssid="Router SSID"
-desc_router_password="Router Password"
-desc_bonjour_name="Bonjour service name"
-desc_ed_tcpport="ED Throttle Service port"
-desc_grid_enable="Cangrid enable"
 desc_grid_port="Grid connect Service port"
 desc_logfile="Logfile"
 desc_logappend="Append log"
 desc_loglevel="Log Level"
 desc_canid="Can ID"
-desc_turnout_file="Turnouts"
-desc_fns_momentary="Fns momentary"
 desc_create_logfile="Creates log file"
 desc_start_event="Start event id"
 desc_btn_save="btnSave"
-desc_btn_apply="btnApply"
 desc_btn_restart="btnRestart"
 desc_btn_stop="btnStop"
 desc_btn_restart_all="btnRestartAll"
 desc_btn_update_file="btnUpdateFile"
-desc_btn_shutdown="btnShutdown"
-desc_orphan_timeout="Orphan sessions timeout seconds"
-desc_shutdown_code="Shutdown code"
-
-#validators
-ssid_length = form.Validator("SSID length should be between 1 and 8 characters", lambda p: p is None or len(p)>8)
-passwd_length = form.Validator("Password length should be 8 characters", lambda p: len(p)!= 8)
-router_ssid_length = form.Validator("Router SSID length should be between 1 and 12 characters", lambda p: p is None or len(p)>12)
-router_passwd_length = form.Validator("Router password length should be less then 12 characters", lambda p: len(p)> 12)
-turnout_length = form.Validator("Turnout name length should less than 11 characters", lambda p: p is None or len(p)>11)
-service_name_length = form.Validator("Service name length should less than 8 characters", lambda p: p is None or len(p)>8)
-
-
-def configurePi():
-    #time.sleep(3)
-    os.system("/etc/init.d/start_canpi.sh configure")
 
 def restartAll():
     #time.sleep(3)
@@ -202,37 +157,15 @@ class index:
                 writeMessage(msg)
                 raise web.seeother('/')
 
-            cm.setValue("ap_mode", str(form[id_apmode].checked))
-            cm.setValue("ap_no_password", str(form[id_apmode_no_passwd].checked))
-            cm.setValue("ap_ssid", str(form[id_ssid].value))
-            cm.setValue("ap_password", str(form[id_password].value))
-            cm.setValue("ap_channel", str(form[id_channel].value))
-            cm.setValue("router_ssid", str(form[id_router_ssid].value))
-            cm.setValue("router_password", str(form[id_router_password].value))
-            cm.setValue("can_grid", str(form[id_grid_enable].checked))
             cm.setValue("cangrid_port", str(form[id_grid_port].value))
-            cm.setValue("service_name", str(form[id_bonjour_name].value))
-            cm.setValue("tcpport", str(form[id_ed_tcpport].value))
             cm.setValue("loglevel", str(form[id_loglevel].value))
             cm.setValue(id_create_logfile, str(form[id_create_logfile].checked))
             cm.setValue(id_logappend, str(form[id_logappend].checked))
             cm.setValue("canid", str(form[id_canid].value))
-            cm.setValue("fn_momentary", str(form[id_fns_momentary].value))
-            cm.setValue("turnout_file", str(form[id_turnout_file].value))
             cm.setValue(id_start_event_id, str(form[id_start_event_id].value))
-            cm.setValue(id_orphan_timeout, str(form[id_orphan_timeout].value))
-            cm.setValue(id_shutdown_code, str(form[id_shutdown_code].value))
             cm.saveFile()
             writeMessage("Save successful")
             raise web.seeother('/')
-
-        if id_btn_apply in userData:
-            print("Apply button pressed")
-            writeMessage("")
-            cpid = os.fork()
-            if cpid == 0:
-                configurePi()
-            return render.reboot("Applying changes and rebooting",myuri)
 
         if id_btn_update_file in userData:
             print("Upgrade button pressed")
@@ -245,11 +178,11 @@ class index:
             os.system("/etc/init.d/start_canpi.sh restartcanpi")
             exitcode,out,err = self.get_exitcode_stdout_stderr("ps -ef")
             msg=""
-            if "/home/pi/canpi/canpi" in out:
+            if "/home/pi/canpiserver/canpiserver" in out:
                 msg="Service restarted"
             else:
                 msg="Restart failed"
-            return render.index(form,"Canpi Configuration",msg)
+            return render.index(form,"Canpiserver Configuration",msg)
 
         if id_btn_stop in userData:
             print("Stop button pressed")
@@ -257,11 +190,11 @@ class index:
             os.system("/etc/init.d/start_canpi.sh stopcanpi")
             exitcode,out,err = self.get_exitcode_stdout_stderr("ps -ef")
             msg=""
-            if "/home/pi/canpi/canpi" in out:
+            if "/home/pi/canpiserver/canpiserver" in out:
                 msg="Stop failed"
             else:
                 msg="Stop successfull"
-            return render.index(form,"Canpi Configuration",msg)
+            return render.index(form,"Canpiserver Configuration",msg)
 
         if id_btn_restart_all in userData:
             print("Restart all button pressed")
@@ -269,33 +202,13 @@ class index:
             cpid = os.fork()
             if cpid == 0:
                 restartAll()
-            return render.reboot("Restarting the webpage and canpi",myuri)
+            return render.reboot("Restarting the webpage and canpiserver",myuri)
 
-        if id_btn_shutdown in userData:
-            print("Shutdown button pressed")
-            writeMessage("")
-            cpid = os.fork()
-            if cpid == 0:
-                shutdownPi()
-            return render.reboot("Shuting down the pi",myuri)
-
-        return render.index(form,"Canpi Configuration","")
+        return render.index(form,"Canpiserver Configuration","")
 
     def validate_form(self,form):
         global desc_ed_tcpport
         global desc_grid_port
-
-        if form[id_apmode].checked == True :
-            if len(form[id_ssid].value) > 8 or len(form[id_ssid].value) == 0 :
-                return False, "SSID length should be between 1 and 8 characters"
-
-            if len(form[id_password].value) != 8 and form[id_apmode_no_passwd].checked == False:
-                return False, "Password length should be 8 characters"
-        else:
-            if len(form[id_router_ssid].value) > 12 or len(form[id_router_ssid].value) == 0 :
-                return False, "Router SSID length should be between 1 and 12 characters"
-            if len(form[id_router_password].value) == 0 or len(form[id_router_password].value) > 12 :
-                return False, "Router password length should be less then 12 characters"
 
         if self.isint(form[id_canid].value):
             v = int(form[id_canid].value)
@@ -304,20 +217,6 @@ class index:
         else:
             return False, "The CANID should be between 1 and 110"
 
-        if self.isint(form[id_orphan_timeout].value):
-            v = int(form[id_orphan_timeout].value)
-            if v <= 0 or v > 3600:
-                return False, "The orphan timeout should be between 1 and 3600 seconds"
-        else:
-            return False, "The orphan should be between 1 and 3600 seconds"
-
-        if self.isint(form[id_ed_tcpport].value):
-            v = int(form[id_ed_tcpport].value)
-            if v <= 0 or v > portlimit or v in invalid_ports:
-                return False, desc_ed_tcpport + "[" + str(v) + "] should be between 1 and " + str(portlimit) + " and not be " + str(invalid_ports)
-        else:
-            return False, desc_ed_tcpport + "[" + str(v) + "] should be between 1 and " + str(portlimit) + " and not be " + str(invalid_ports)
-
         if self.isint(form[id_grid_port].value):
             v1 = int(form[id_grid_port].value)
             if v1 <= 0 or v1 > portlimit or v1 in invalid_ports:
@@ -325,8 +224,6 @@ class index:
         else:
             return False, desc_grid_port + " should be between 1 and " + str(portlimit) + " and not be " + str(invalid_ports)
 
-        if v == v1:
-            return False, desc_ed_tcpport + " and " + desc_grid_port + " should be different"
 
         if self.isint(form[id_start_event_id].value):
             v = int(form[id_start_event_id].value)
@@ -336,25 +233,6 @@ class index:
         else:
             return False, desc_start_event + "[" + str(v) + "] should be between 1 and " + str(portlimit)
   
-        if self.isint(form[id_shutdown_code].value):
-            v = int(form[id_shutdown_code].value)
-            if v <= -1 or v > portlimit:
-                print("Run validation\n")
-                return False, desc_shutdown_code + "[" + str(v) + "] should be between -1 and " + str(portlimit)
-            return False, desc_shutdown_code + "[" + form[id_shutdown_code].value + "] should be between -1 and " + str(portlimit)
-
-
-        if self.isint(form[id_orphan_timeout].value):
-            v = int(form[id_orphan_timeout].value)
-            if v <= 0 or v > 120:
-                print("Run validation\n")
-                return False, desc_orphan_timeout + "[" + str(v) + "] should be between 1 and 120 seconds"
-        else:
-            return False, desc_orphan_timeout + "[" + str(v) + "] should be between 1 and and 120 seconds"
-
-        if len(form[id_turnout_file].value) == 0 or len(form[id_turnout_file].value) > 11 :
-            return False, "Turnout file name length should be between 1 and 11"
-
         return True,"Saved success"
 
     def isint(self, value):
@@ -383,42 +261,19 @@ class index:
         create_logfile = True
         logappend = True
 
-        if cm.getValueInsert("ap_mode", "True").lower() != "true":
-            apmode = False
-        if cm.getValueInsert("can_grid", "True").lower() != "true":
-            gridenable = False
-        if cm.getValueInsert(id_apmode_no_passwd, "True").lower() != "true":
-            apmode_no_passwd = False
         if cm.getValueInsert(id_create_logfile, "True").lower() != "true":
             create_logfile = False
         if cm.getValueInsert(id_logappend, "True").lower() != "true":
             logappend = False
 
         myform = form.Form(
-            form.Button(desc_btn_shutdown, id=id_btn_shutdown, value="shutdown", html="Shutdown"),
-            form.Checkbox(id_apmode,description=desc_apmode,checked=apmode,value="apmode",id="tapmode"),
-            form.Checkbox(id_apmode_no_passwd,description=desc_apmode_no_passwd,checked=apmode_no_passwd,value="apmode_no_passwd",id="tapmode_no_passwd"),
-            form.Textbox(id_ssid,ssid_length,description=desc_ssid,value=cm.getValueInsert("ap_ssid","canpiwi"),id="apssid"),
-            form.Textbox(id_password,passwd_length,description=desc_password, value=cm.getValueInsert("ap_password","12345678"),id="appasswd"),
-            form.Dropdown(id_channel, ['1', '2', '3','4','5','6','7','8','9','10','11','12','13'],value=cm.getValueInsert("ap_channel",6)),
-            form.Textbox(id_router_ssid,router_ssid_length,description=desc_router_ssid,value=cm.getValueInsert("router_ssid",""),id="routerssid"),
-            form.Textbox(id_router_password,router_passwd_length, description=desc_router_password,value=cm.getValueInsert("router_password",""),id="routerpasswd"),
-            form.Textbox(id_bonjour_name,service_name_length,description=desc_bonjour_name,value=cm.getValueInsert("service_name","MyLayout"),id="servicename"),
-            form.Textbox(id_ed_tcpport,description=desc_ed_tcpport,value=cm.getValueInsert("tcpport",5555),id="tcpport"),
-            form.Checkbox(id_grid_enable,description=desc_grid_enable,checked=gridenable,value="gridenable",id="cangrid"),
             form.Textbox(id_grid_port,description=desc_grid_port,value=cm.getValueInsert("cangrid_port",4444),id="cangripport"),
             form.Textbox(id_canid,description=desc_canid,value=cm.getValueInsert("canid",100)),
             form.Textbox(id_start_event_id,description=desc_start_event,value=cm.getValueInsert(id_start_event_id,1)),
-            form.Textbox(id_orphan_timeout,description=desc_orphan_timeout,value=cm.getValueInsert(id_orphan_timeout,30)),
-            form.Textbox(id_shutdown_code,description=desc_shutdown_code,value=cm.getValueInsert(id_shutdown_code,-1)),
-            form.Textbox(id_fns_momentary,description=desc_fns_momentary,value=cm.getValueInsert("fn_momentary","2")),
-            form.Textbox(id_turnout_file,turnout_length,description=desc_turnout_file,value=cm.getValueInsert("turnout_file","turnout.txt")),
             form.Dropdown(id_loglevel,  ['INFO', 'WARN', 'DEBUG'],value=cm.getValueInsert("loglevel","INFO")),
             form.Checkbox(id_create_logfile,description=desc_create_logfile,checked=create_logfile,value=id_create_logfile,id="tcreate_logfile"),
             form.Checkbox(id_logappend,description=desc_logappend,checked=logappend,value= id_logappend,id="tlogappend"),
-            #form.Textbox(id_logfile,description=,value=cm.getValue("logfile"),id="logfile"),
             form.Button(desc_btn_save, id=id_btn_save, value="save", html="Save changes"),
-            form.Button(desc_btn_apply, id=id_btn_apply, value="apply", html="Apply changes and reboot"),
             form.Button(desc_btn_stop, id=id_btn_stop, value="stop", html="Stop throttle service"),
             form.Button(desc_btn_restart_all, id=id_btn_restart_all, value="stop", html="Restart throttle and configuration page"),
             form.Button(desc_btn_restart, id=id_btn_restart, value="restart", html="Restart throttle service"),
